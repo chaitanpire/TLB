1c7e2703290a5d4d18fee43a226d8e3c5ec6b359
Chaitanya Garg <22b0979@iitb.ac.in>
2024-10-26 05:05:07 +0530

    Dead End

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
-./build_champsim.sh srrip 1 
-./build_champsim.sh lru 1
+# Loop over each trace file in the traces directory
 for i in `ls -1 ./traces/`; do
-    ./bin/srrip-1core -warmup_instructions 25000000 -simulation_instructions 25000000 -traces ./traces/$i > ./logs/${i}_srrip.txt
-    ./bin/lru-1core -warmup_instructions 25000000 -simulation_instructions 25000000 -traces ./traces/$i > ./logs/${i}_lru.txt
+    # Run each configuration and save output to a unique log file
+    mkdir ./logs/${i}
+    ./bin/srrip-1core -warmup_instructions 25000000 -simulation_instructions 25000000 -traces ./traces/$i > ./logs/$i/srrip.txt
+    ./bin/srrip_cost_aware_max-1core -warmup_instructions 25000000 -simulation_instructions 25000000 -traces ./traces/$i > ./logs/${i}/srrip_cost_aware_max.txt
+    ./bin/srrip_cost_aware_min-1core -warmup_instructions 25000000 -simulation_instructions 25000000 -traces ./traces/$i > ./logs/${i}/srrip_cost_aware_min.txt
+    mv collison.txt ./logs/$i/
 done
